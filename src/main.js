bf7afe85c12c1454a09268746bff8b14f3e26689
Lucia Gigena <gigenalucian@gmail.com>
2022-09-03 12:45:38 -0300

    first

---
+++ src/main.js
@@ -0,0 +1,103 @@
+import express from 'express'
+
+import { Server as HttpServer } from 'http'
+import { Server as Socket } from 'socket.io'
+
+import ContenedorSQL from './contenedores/ContenedorSQL.js'
+import ContenedorArchivo from './contenedores/ContenedorArchivo.js'
+
+import config from './config.js'
+
+//--------------------------------------------
+// instancio servidor, socket y api
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+const productosApi = new ContenedorSQL(config.mariaDb, 'productos')
+const mensajesApi = new ContenedorArchivo(`${config.fileSystem.path}/mensajes.json`)
+
+//--------------------------------------------
+// NORMALIZACIÓN DE MENSAJES
+
+import { normalize, schema, } from 'normalizr'
+
+// Definimos un esquema de autor
+const schemaAuthor = new schema.Entity('author', {}, { idAttribute: 'email' });
+
+// Definimos un esquema de mensaje
+const schemaMensaje = new schema.Entity('post', { author: schemaAuthor }, { idAttribute: 'id' })
+
+// Definimos un esquema de posts
+const schemaMensajes = new schema.Entity('posts', { mensajes: [schemaMensaje] }, { idAttribute: 'id' })
+
+const normalizarMensajes = (mensajesConId) => normalize(mensajesConId, schemaMensajes)
+
+//--------------------------------------------
+// configuro el socket
+
+io.on('connection', async socket => {
+    console.log('Nuevo cliente conectado!');
+
+    // carga inicial de productos
+    socket.emit('productos', await productosApi.listarAll());
+
+    // actualizacion de productos
+    socket.on('update', async producto => {
+        await productosApi.guardar(producto)
+        io.sockets.emit('productos', await productosApi.listarAll());
+    })
+
+    // carga inicial de mensajes
+    socket.emit('mensajes', await listarMensajesNormalizados());
+
+    // actualizacion de mensajes
+    socket.on('nuevoMensaje', async mensaje => {
+        mensaje.fyh = new Date().toLocaleString()
+        await mensajesApi.guardar(mensaje)
+        io.sockets.emit('mensajes', await listarMensajesNormalizados());
+    })
+});
+
+async function listarMensajesNormalizados() {
+    const mensajes = await mensajesApi.listarAll()
+    const normalizados = normalizarMensajes({ id: 'mensajes', mensajes })
+    return normalizados
+}
+
+//--------------------------------------------
+// agrego middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+//--------------------------------------------
+
+import faker from 'faker'
+faker.locale = 'es'
+
+app.get('/api/productos-test', (req, res) => {
+    const CANT_PRODS = 5
+    const productos = []
+    for (let i = 1; i <= CANT_PRODS; i++) {
+        const prod = {
+            id: i,
+            title: faker.commerce.product(),
+            price: faker.commerce.price(),
+            thumbnail: `${faker.image.imageUrl()}?${i}`
+        }
+        productos.push(prod)
+    }
+    res.json(productos)
+})
+
+//--------------------------------------------
+// inicio el servidor
+
+const PORT = 8080
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
